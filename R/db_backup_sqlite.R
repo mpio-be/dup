0ec3e0ccb745cfbd16a15e80317aa99395867fd1
mihai <valcu@orn.mpg.de>
2022-03-03 11:11:49 +0100

    -

--- R/db_backup_sqlite.R
+++ R/db_backup_sqlite.R
@@ -4,6 +4,7 @@
 #' @param  tables  one of more table names, character vector
 #' @param  exclude_columns  which columns to exclude
 #' @param  indices  which columns are indices
+#' @param datetime_col which column is datetime
 #' @param  dir    directory path. the destination file (same location on both local and remote).
 #' @param  filename   name of the sqlite file.
 #' @param  remote when TRUE the file is uploaded to a remote host defined in cnf
@@ -21,12 +22,13 @@
 #'  Sys.setenv(R_CONFIG_ACTIVE = "localhost")
 #' sqlitedump(db = 'ARGOS',
 #' tables = c('2019_LBDO', '2020_BADO', '2022_WRSA'), 
-#' exclude_columns = c("satellite",  "messageDate", "locationClass", "compressionIndex",  "S1", "S2", "S3", "S4", "S5", #' "S6", "S7", "S8", "filenam"), 
+#' exclude_columns = c("satellite",  "messageDate", "locationClass", "compressionIndex",  "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "filenam", "pk") , 
 #' indices = c('tagID', 'locationDate')
+#' datetime_cols = 'locationDate'
 #' )
 #' }
 #' 
-sqlitedump <- function(db, tables,exclude_columns, indices,
+sqlitedump <- function(db, tables,exclude_columns, indices,datetime_cols,
                       dir = '.',
                       filename = glue("{dir}/{db}.sqlite"), 
                       remote = TRUE,
@@ -45,23 +47,30 @@ sqlitedump <- function(db, tables,exclude_columns, indices,
          on.exit(dbDisconnect(con))
 
     xl = foreach(i=1:length(tables)) %do% {
-        ti = dbReadTable(con, tables[i])
-        setDT(ti)
+      ti = dbReadTable(con, tables[i]) 
+      setDT(ti)
       if (!missing(exclude_columns)) {
         ti[, (exclude_columns) := NULL]
       }
+      
+      ti = unique(ti)    
 
       if (!missing(fun)) {
         fun(ti)
       }
-
+  
+      if (!missing(datetime_col)) {
+        ti[, (datetime_col) := as.character(datetime_col)]
+      }
+      
+      
       ti
       }
 
     dbnam = paste(dir, filename, sep = "/")
     if (file.exists(dbnam)) file.remove(dbnam)
 
-    conlite = dbConnect(RSQLite::SQLite(), dbname = paste(dir, filename,sep='/'))
+    conlite = dbConnect(RSQLite::SQLite(), dbname = paste(dir, filename,sep='/'), extended_types = TRUE)
 
     foreach(i = 1:length(tables) ) %do% {
       dbWriteTable(conlite, tables[i], xl[[i]], row.names = FALSE)
@@ -72,6 +81,11 @@ sqlitedump <- function(db, tables,exclude_columns, indices,
             glue("CREATE INDEX {indices[j]}_{tables[i]}_index ON `{tables[i]}` ( {indices[j]});")
           )
       }
+      
+      if (!missing(datetime_col)) {
+        ti[, (datetime_col) := as.character(datetime_col)]
+      }      
+      
 
     }
 
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # dup
+
 Database Unattended Pipelines
 
 
