b70ca4ef33986ebfb57a4e2d6e10a8c6cbac0b9b
Mihai Valcu <valcu@orn.mpg.de>
2023-10-19 16:55:42 +0200

    safe fetch one hour earlier to prevent data loass

--- R/DRUID.R
+++ R/DRUID.R
@@ -1,3 +1,11 @@
+to_timestamp <- function(x) {
+  format(x, format = "%Y-%m-%dT%H:%M:%SZ")
+  }
+
+from_timestamp <- function(x) {
+  with_tz(ymd_hms(x), "UTC")
+  }
+
 
 
 #' Download the latest DRUID data and updates DB
@@ -23,13 +31,20 @@ DRUID.downloadNew <- function(what) {
   d[program_id == "aaulsebrook", lstr := logA]
   d[program_id == "luke.eberhart", lstr := logL]
 
-  d[, last_timestamp := format(last_timestamp, format = "%Y-%m-%dT%H:%M:%SZ")]
-
-
-
+ 
   o <- foreach(i = 1:nrow(d), .errorhandling = "pass") %do% {
     if (interactive())  print(i)
-    x <- ecotopia_data(d[i, lstr], d[i, id], datetime = d[i, last_timestamp], what = tolower(what) , verbose = FALSE)
+    
+    
+    dtm = d[i, last_timestamp]
+
+    oi = ecotopia_data(d[i, lstr], d[i, id],
+      datetime = to_timestamp(dtm - 3600), # fetch one hour earlier to prevent data loss
+      what = tolower(what),
+      verbose = FALSE
+    )
+    
+    oi[from_timestamp(timestamp) > dtm]
 
   }
 
@@ -42,8 +57,8 @@ DRUID.downloadNew <- function(what) {
     O[latitude == 200, ":="(latitude = NA, longitude = NA, altitude = NA, hdop = NA, vdop = NA)]
  
   setnames(O, "device_id", "id")
-  O[, updated_at := with_tz(ymd_hms(updated_at), "UTC")]
-  O[, timestamp := with_tz(ymd_hms(timestamp), "UTC")]
+  O[, updated_at := from_timestamp(updated_at) ]
+  O[,  timestamp :=  from_timestamp(timestamp) ]
   
   con = dbcon(db = "DRUID", server = "scidb")
   ok = DBI::dbWriteTable(con, what, O, append = TRUE, row.names = FALSE)
